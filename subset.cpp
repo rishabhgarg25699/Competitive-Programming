627759950274547de5454f7d35238cca9fbe0bac
yashica-patodia <yashica1002@gmail.com>
2019-12-15 23:13:47 +0530

    SUBSETS OF A SET

--- subset.cpp
+++ subset.cpp
@@ -6,7 +6,8 @@ using namespace std;
 //or not
 void subset(vector<int>& arr,vector<vector<int>>& result,vector<int>& sub,int i)
 {
-	int k;
+	int k,p,j;
+	if(i!=0)
 	result.push_back(sub);//pushing the formed subset into the 2-d vector
 	for(k=i;k<arr.size();k++)
 	{       
@@ -14,17 +15,28 @@ void subset(vector<int>& arr,vector<vector<int>>& result,vector<int>& sub,int i)
 		subset(arr,result,sub,k+1);//recursive along that edge of tree
 		sub.pop_back();//poping the element
 	}
+return ;
 }
 int main()
 {
-	int i,j;
-	vector <int>arr={1,2,3};
+	int i,j,n,v;
+   	cout<<"enter the number of elements"<<endl;
+	cin>>n;
+	vector<int>arr(n);
+	for(i=0;i<n;i++)
+	{
+	cout<<"enter the element"<<endl;
+	cin>>v;
+	arr[i]=v;
+	}
+	
 	vector<vector<int>>result;
 	vector<int>sub;
 	subset(arr,result,sub,0);
 	for(i=0;i<result.size();i++)
 	{
 		for(j=0;j<result[i].size();j++)
+
 		cout<<result[i][j]<<" ";
 		cout<<endl;
 	}
