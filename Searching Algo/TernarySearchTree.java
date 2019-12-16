05b87f14aaf85e07d2bcfb15e3ece76660e8c4b6
Knlsharma <knlsharma678@gmail.com>
2019-12-16 20:33:52 +0530

    indented added

--- Searching Algo/TernarySearchTree.java
+++ Searching Algo/TernarySearchTree.java
@@ -71,7 +71,8 @@ public class TernarySearchTree<Value> {
 
     private Node<Value> put(Node<Value> x, String key, Value val, int d) {
         char c = key.charAt(d);
-        if (x == null) {
+        if (x == null) 
+		{
             x = new Node<>();
             x.c = c;
         }
@@ -86,7 +87,7 @@ public class TernarySearchTree<Value> {
 		}
         else if (d < key.length() - 1) 
 		{
-			  x.mid   = put(x.mid,   key, val, d + 1);
+			 x.mid   = put(x.mid,   key, val, d + 1);
 		}
         else 
         {	
