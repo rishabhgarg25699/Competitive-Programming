a861103ff84eefbb5d585d95cbbaf9d73bcd09c5
ashishugi <46626591+ashishugi@users.noreply.github.com>
2019-12-19 17:42:59 +0530

    Update addition.cpp

--- addition.cpp
+++ addition.cpp
@@ -33,7 +33,7 @@ struct node *add(struct node *p,struct node *q,struct node *first)// Addition of
   {
     if(p->expo==q->expo)
     {
-      first=insert(first,(p->coef+q->coef),p->expo);
+       first=insert(first,(p->coef+q->coef),p->expo);
        p=p->next;
        q=q->next;
     }
@@ -48,20 +48,22 @@ struct node *add(struct node *p,struct node *q,struct node *first)// Addition of
      q=q->next;
     }
   }// end of the while loop.
-  while(p!=NULL)
+  
+ while(p!=NULL)
   {
    insert(first,p->coef,p->expo);
    p=p->next;
   }
   while(q!=NULL)
   {
-  insert(first,q->coef,q->expo);
-  q=q->next;
+   insert(first,q->coef,q->expo);
+   q=q->next;
   }
   return first;
 } // end of the ADD function.
 void display(struct node *first)
-{    struct node *ptr;
+{    
+ struct node *ptr;
  for(ptr=first;ptr!=NULL;ptr=ptr->next)
  {
    cout<<ptr->coef;
@@ -78,32 +80,33 @@ int main()
   int coef,expo;
   while(ch=='y')
   {
-  cout<<"enter the coefficient"<<endl;
-  cin>>coef;
-  cout<<"enter the exponent"<<endl;
-  cin>>expo;
-  firstp=insert(firstp,coef,expo);
-  cout<<"Do you want to enter more Y/N:";
-  cin>>ch;
-  }              ch='y';
+   cout<<"enter the coefficient"<<endl;
+   cin>>coef;
+   cout<<"enter the exponent"<<endl;
+   cin>>expo;
+   firstp=insert(firstp,coef,expo);
+   cout<<"Do you want to enter more Y/N:";
+   cin>>ch;
+  }
+  ch='y';
   cout<<"\n Now for the 2nd equation";
   while(ch=='y')
   {
-  cout<<"enter the coefficient : "<<endl;
-  cin>>coef;
-  cout<<"enter the exponent:"<<endl;
-  cin>>expo;
-  firstq=insert(firstq,coef,expo);
-  cout<<"Do you want to enter more Y/N:"<<endl;
-  cin>>ch;
+   cout<<"enter the coefficient : "<<endl;
+   cin>>coef;
+   cout<<"enter the exponent:"<<endl;
+   cin>>expo;
+   firstq=insert(firstq,coef,expo);
+   cout<<"Do you want to enter more Y/N:"<<endl;
+   cin>>ch;
  }
    firstr=add(firstp,firstq,firstr);
    cout<<"the first euation is: "<<endl;
    display(firstp);
    cout<<" the 2nd equation is:\t"<<endl;
-  display(firstq);
-    cout<<"the resultant equation is : \t"<<endl;
-    display(firstr);
+   display(firstq);
+   cout<<"the resultant equation is : \t"<<endl;
+   display(firstr);
 
     
 }
