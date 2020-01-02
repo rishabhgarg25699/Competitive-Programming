dee4a2884e287021ee68e2798b21ddfc4d6de50e
Srijanak De <54339186+srijanakde2001@users.noreply.github.com>
2020-01-02 22:38:21 +0530

    Update 3

--- smsubset.cpp
+++ smsubset.cpp
@@ -2,11 +2,14 @@
 using namespace std;
 int g=21,garr[20];
 void subarray(int *arr, int n, int x, int ans[], int m)
-{if(x==0 && n>=-1)
-  {if(m<=g)
-     {g = m;
+{
+ if(x==0 && n>=-1)
+  {
+    if(m<=g)
+     {
+	  g = m;
       for(int j=0;j<g;j++)
-	garr[j] = ans[j];
+	    garr[j] = ans[j];
      }
     return;
   }
@@ -18,19 +21,24 @@ void subarray(int *arr, int n, int x, int ans[], int m)
  return;
 }
 int main()
-{int n,x,i,j=0,arr[20],ans[20],m=0,narr[20];
+{
+ int n,x,i,j=0,arr[20],ans[20],m=0,narr[20];
  cin>>n>>x;
  for(i=n-1;i>=0;i--)
-  {cin>>arr[i];
+  {
+   cin>>arr[i];
    narr[n-1-i] = arr[i];
   }
  subarray(arr,n-1,x,ans,m);
  if(g==21)
   cout<<"-1";
  else
-  {for(i=0;i<n;i++)
-     {if(narr[i]==garr[j])
-	    {if(j==0)	
+  {
+   for(i=0;i<n;i++)
+     {
+	  if(narr[i]==garr[j])
+	    {
+		 if(j==0)	
 	      cout<<i+1<<" ";
 	     else
            cout<<i+1<<" ";
@@ -41,4 +49,4 @@ int main()
      }
   }  
  return 0;	
- }
+}
