13eb9e5d03c6175883d1ddb075b35cfdf1ed5634
Aryan Maurya <52876431+aryanmaurya1@users.noreply.github.com>
2020-10-01 20:09:25 +0530

    Delete Quicksort.hs

--- Sorting Algos/Quicksort.hs
+++
@@ -1,8 +0,0 @@
-module Quicksort where
-
-
-qsort [] = []
-qsort (x:xs) = qsort ys ++ [x] ++ qsort zs
-           where
-             ys = [a | a <- xs, a <= x]
-             zs = [b | b <- xs, b > x]
